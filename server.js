23a5a4af79deabec6f8d4acd0aef18ef31c49cea
jyothi91 <jyonew91@gmail.com>
2016-09-20 08:13:16 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/artical-one', function (req, res) {
+  res.sendFile('Artical one is requested and will be served here');
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
