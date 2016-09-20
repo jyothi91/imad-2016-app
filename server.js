2c3d42350b27c6091a750b52adca05027080ae99
jyothi91 <jyonew91@gmail.com>
2016-09-20 19:54:22 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,28 +26,33 @@ var articalone{
                 This is a content of my first article.This is a content of my first article.This is a content of my first article.
             </p>`;
 }
-var htmltemplate=`<html>
-    <head>
-        <title>
-            ${title};
-        </title>
-        <meta name= "viewport" content="width=device.width,initial-scale=1"/>
-        <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class = "container">
-            <div>
-                <a href="/">Home</a>
+function createtemplate(data){
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var htmltemplate=`<html>
+        <head>
+            <title>
+                ${title};
+            </title>
+            <meta name= "viewport" content="width=device.width,initial-scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class = "container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h3>${heading};</h3>
+                <div>${date};</div>
+                ${content};
             </div>
-            <hr/>
-            <h3>${heading};</h3>
-            <div>${date};</div>
-            ${content};
-        </div>
-    </body>
-    
-</html>
-`
+        </body>
+        
+    </html>
+    `
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
