0d17e589263a77a4387d383fb1e31d484fc3b330
jyothi91 <jyonew91@gmail.com>
2016-09-28 15:44:13 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function(){
     //make a request to the counter and point
-    
+    var request = new XMLHttpRequest();
     //capture the response and store it in a variable
     
     //Render the variable in the correct span
