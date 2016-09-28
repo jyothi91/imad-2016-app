3ebc0ebfcdf559b033dc3eb7ad63457ffe9bf849
jyothi91 <jyonew91@gmail.com>
2016-09-28 17:44:39 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,10 +37,17 @@ button.onclick = function(){
         {
             if(request.status === 200)
             {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
+              //capture a list of names and render it as a list
+             var names = ['name1','name2','name3','name4'];
+             var list = '';
+             for (var i = 0;i<names.length;i++)
+             {
+                 list += '<li>' + names[i] + '</li>';
+                 
+             }
+             var ul = document.getElementById('namelist');
+             ul.innerHTML = list;
+                    }
         }
         
     };
@@ -48,15 +55,6 @@ button.onclick = function(){
     //make a request
     request.open('GET','http://jyothi91.imad.hasura-app.io/counter',true);
     request.send(null);     
-     //capture a list of names and render it as a list
-     var names = ['name1','name2','name3','name4'];
-     var list = '';
-     for (var i = 0;i<names.length;i++)
-     {
-         list += '<li>' + names[i] + '</li>';
-         
-     }
-     var ul = document.getElementById('namelist');
-     ul.innerHTML = list;
+
  };
  
