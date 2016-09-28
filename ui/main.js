d44c54fa99c2ed8674448ba251b56962fd9b1836
jyothi91 <jyonew91@gmail.com>
2016-09-28 08:04:15 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,10 +7,10 @@ var img = document.getElementById('madi');
 
 var marginleft = 0;
 function moveright(){
-    marginleft =marginleft + 10;
+    marginleft =marginleft + 1;
     img.style.marginLeft = marginleft + 'px';
 }
 img.onclick = function(){
      //img.style.marginLeft =  '200px';
-   var interval = setInterval(moveright,100);
+   var interval = setInterval(moveright,50);
 };
