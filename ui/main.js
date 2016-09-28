a71e15e664a154bfe1a1737348d6be887cf41cf7
jyothi91 <jyonew91@gmail.com>
2016-09-28 16:00:14 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,22 +2,22 @@ var button = document.getElementById('counter');
 button.onclick = function(){
     alter('he');
     //create a request to the counter and point
-    var request = new XMLHttpRequest();
+  //  var request = new XMLHttpRequest();
     //capture the response and store it in a variable
-    request.onreadystatechange = function(){
-        if (request.readystate === XMLHttpRequest.DONE )
-        {
-            if(request.status === 200)
-            {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
+    //request.onreadystatechange = function(){
+      //  if (request.readystate === XMLHttpRequest.DONE )
+        //{
+          //  if(request.status === 200)
+            //{
+              //  var counter = request.responseText;
+                //var span = document.getElementById('count');
+                //span.innerHTML = counter.toString();
+            //}
+        //}
         
-    };
+//    };
     
     //make a request
-    request.open('GET','http://jyothi91.imad.hasura-app.io/counter',true);
-    request.send(null);
+  //  request.open('GET','http://jyothi91.imad.hasura-app.io/counter',true);
+    //request.send(null);
   };
