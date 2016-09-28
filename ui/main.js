d02ae5bdff22907701522840ed399ce4a38ad50f
jyothi91 <jyonew91@gmail.com>
2016-09-28 18:02:25 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -39,10 +39,11 @@ button.onclick = function(){
               //capture a list of names and render it as a list
              var names =request.responseText;
              names = JSON.parse(names);
+             alert(names);
              var list = '';
              for (var i = 0;i<names.length;i++)
              {
-                 alert(names[i]);
+                 
                  list += '<li>' + names[i] + '</li>';
                  
              }
