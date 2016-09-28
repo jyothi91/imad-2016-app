61b69947c405e3084a0390d637aedaaf8c29b562
jyothi91 <jyonew91@gmail.com>
2016-09-28 15:56:52 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 var button = document.getElementById('counter');
-button.onclick = function(){
+button.onClick = function(){
     alter('he');
     //create a request to the counter and point
     var request = new XMLHttpRequest();
