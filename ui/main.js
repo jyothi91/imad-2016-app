9bf1ccf14547217b14a1b2c62b80e5c8f815cd3b
jyothi91 <jyonew91@gmail.com>
2016-09-28 07:42:55 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,11 +5,12 @@ element.innerHTML = 'new value ';
 //move the image
 var img=document.getElementById('madi');
 
-var marginleft = 0;
-function moveright(){
-    marginleft =marginleft + 10;
-    img.style.marginleft = marginleft + 'px';
-}
+//var marginleft = 0;
+//function moveright(){
+ //   marginleft =marginleft + 10;
+   // img.style.marginleft = marginleft + 'px';
+//}
 img.onclick = function(){
-    var interval = SetInterval(moveright,100);
+     img.style.marginleft =  '100px';
+  //  var interval = SetInterval(moveright,100);
 };
