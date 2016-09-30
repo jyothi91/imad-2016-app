fc22c3d358f82ba3a5739ae4973f99441419c821
jyothi91 <jyonew91@gmail.com>
2016-09-30 20:14:50 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,10 +74,13 @@ function createtemplate(data){
                 ${content};
             <hr/>
             <br/>
+            if (${heading} === Article one)
+            {
                 <input type= "text" id="comment" placeholder = "comment" ></input>
                 <input type = "submit" value = "Submit" id = 'submit_btn'></input>
                 <ul id = 'commentlist'>
                 </ul>
+            }
            </div>
         </body>
         
