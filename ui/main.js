3f273cd1c4bec99da984b0426f83dca2c5402305
jyothi91 <jyonew91@gmail.com>
2016-09-30 21:10:35 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,8 +15,6 @@
             
             if(request.status === 200)
             {
-                alert(request.status);
-                
               //capture a list of names and render it as a list
              var commets = request.responseText;
              commets = JSON.parse(commets);
