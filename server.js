69ae84caceb16082bdd9cbedab9bd4f4d42f48ad
jyothi91 <jyonew91@gmail.com>
2016-09-30 20:09:55 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,10 +74,13 @@ function createtemplate(data){
                 ${content};
             <hr/>
             <br/>
-            <input type= "text" id="comment" placeholder = "comment" id = 'name'></input>
-            <input type = "submit" value = "Submit" id = 'submit_btn'></input>
-            <ul id = 'namelist'>
-            </ul>
+            if (${heading} === 'Article one')
+            {
+                <input type= "text" id="comment" placeholder = "comment" ></input>
+                <input type = "submit" value = "Submit" id = 'submit_btn'></input>
+                <ul id = 'commentlist'>
+                </ul>
+            }
            </div>
         </body>
         
