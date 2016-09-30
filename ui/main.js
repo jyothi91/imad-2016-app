936aceff923c3daa89bb6cd4503194d9f26d051e
jyothi91 <jyonew91@gmail.com>
2016-09-30 23:06:03 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,11 +9,11 @@
     var request = new XMLHttpRequest();
     //capture the response and store it in a variable
     request.onreadystatechange = function(){
-       alert('list');
+      
         
         if (request.readyState === XMLHttpRequest.DONE )
         {
-            
+           alert('list');  
             if(request.status === 200)
             {
         
