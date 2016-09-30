94030c427336b07b7ef4d8dce18a4b4f3d165d76
jyothi91 <jyonew91@gmail.com>
2016-09-30 20:21:50 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -62,3 +62,44 @@ button.onclick = function(){
 
  };
  
+ 
+  // submit name
+ var submit1 = document.getElementById('submit_btn');
+ submit1.onclick = function(){
+     //make a request to the server and send the name
+    alert('This is article one');
+  //create a request to the counter and point
+  /*  var request = new XMLHttpRequest();
+    //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE )
+        {
+            if(request.status === 200)
+            {
+                
+              //capture a list of names and render it as a list
+             var names = request.responseText;
+             names = JSON.parse(names);
+             var list = '';
+             for (var i = 0;i<names.length;i++)
+             {
+                 
+                 list += '<li>' + names[i] + '</li>';
+                 
+             }
+             var ul = document.getElementById('namelist');
+             ul.innerHTML = list;
+                    }
+        } */
+        
+    };
+    
+    //var nameInput = document.getElementById('name');
+    //var name = nameInput.value;
+
+    //make a request
+    //request.open('GET','http://jyothi91.imad.hasura-app.io/submit-name?name= '+ name,true);
+    //request.send(null);     
+
+ };
+ 
