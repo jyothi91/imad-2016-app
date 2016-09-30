8264cc36c872ff2017893e536ee033b900ed7262
jyothi91 <jyonew91@gmail.com>
2016-09-30 22:46:47 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -92,7 +92,7 @@ button.onclick = function(){
             
              names = JSON.parse(names);
              var list = '';
-             alert(names.length);
+             alert(names);
              for (var i = 0;i<names.length;i++)
              {
                  
