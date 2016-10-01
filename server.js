29e817801bda103c5c209ed49ee2c50729740d38
jyothi91 <jyonew91@gmail.com>
2016-10-01 11:34:44 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ var articles={
                     This is a content of my first article.This is a content of my first article.This is a content of my first article.
                     This is a content of my first article.This is a content of my first article.This is a content of my first article.
                     This is a content of my first article.This is a content of my first article.This is a content of my first article.
-                </p>`,
+                </p>`
 
 
 /*      'article-one' : {
