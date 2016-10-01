e3ecb7df33cba9b33e0f96f55949478703dd00de
jyothi91 <jyonew91@gmail.com>
2016-10-01 11:47:19 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -49,7 +49,7 @@
     //make a request
     //xhttp.open("GET", "demo_get2.asp?fname=Henry&lname=Ford", true);
     
-    request.open('GET','http://jyothi91.imad.hasura-app.io/article1name-one?commentstr= '+ commentstr,true);
+    request.open('GET','http://jyothi91.imad.hasura-app.io/article-one?commentstr= '+ commentstr,true);
     request.send(null);     
    // alert ('http://jyothi91.imad.hasura-app.io/article1-one?commentstr= ' && commentstr );
 
