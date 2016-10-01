b3d2a84ecfb77e9cd71a118193ccd64678c03e95
jyothi91 <jyonew91@gmail.com>
2016-10-01 08:22:41 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ function createtemplate(data){
             <hr/>
             <br/>
             <input type= "text" id="comment5" placeholder = "comment" ></input>
-            <input type = "submit" value = "Submit" id = 'submit_btn1'></input>
+            <input type = "submit" value = "Submit" id = 'submit_btn5'></input>
             <ul id = 'commentlist'>
             </ul>
            </div>
