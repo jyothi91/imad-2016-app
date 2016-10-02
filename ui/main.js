51704b9acc1a3aeddda8a027d229d46db58f4e49
jyothi91 <jyonew91@gmail.com>
2016-10-02 17:09:39 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,8 +19,8 @@
             {
         
               //capture a list of names and render it as a list
-             var comments = request.responsetext;
-             alert(request.responsetext);
+             var comments = request.responseText;
+             alert(request.responseText);
             alert('request status 200');
             // alert(comments);
              //comments = JSON.parse(comments);
