ac26b2c3b1fe892bbdccca7df74324d56df6c461
jyothi91 <jyonew91@gmail.com>
2016-10-24 12:00:19 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config={
     user : 'jyothi91',
     database : 'jyothi91',
     host : 'db.imad.hasura-app.io',
-    port : 'S432',
+    port : '5432',
     password : process.env.DB_PASSWORD
 };
 var app = express();
