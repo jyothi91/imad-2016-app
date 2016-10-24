5b5e4d66290a183ff7685a95d5b7e9ba2abe583a
jyothi91 <jyonew91@gmail.com>
2016-10-24 12:40:01 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-//var pool = new pool(config);
+var pool = new pool(config);
 app.get('/test-db',function(req,res){
     res.send('test');
         pool.query('SELECT * FROM test',function(err,result){
