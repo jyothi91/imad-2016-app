dfad86b9e50cfe888a6a1d15e632e84f26e404da
jyothi91 <jyonew91@gmail.com>
2016-10-24 12:32:59 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ var morgan = require('morgan');
 var path = require('path');
 var pool =require('pg').pool;
 
-/*var config={ 
+var config={ 
     user : 'jyothi91',
     database : 'jyothi91',
     host : 'db.imad.hasura-app.io',
     port : '5432',
     password : process.env.DB_PASSWORD
-};*/
+};
 var app = express();
 app.use(morgan('combined'));
 
