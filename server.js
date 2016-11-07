d336df75d8be7cb90f116bcea5fb72b7c51ed60f
jyothi91 <jyonew91@gmail.com>
2016-11-07 12:25:27 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -143,7 +143,7 @@ app.get('/articles/:articlename', function (req, res) {
   // var commentstr = req.query.commentstr; //to do
    //comments.push(commentstr);
     
-    pool.query("SELECT * FROM article WHERE title = '" + req.params.articlename + "'" ,function(err,result){
+    pool.query("SELECT * FROM article WHERE title = $1", [req.params.articlename] ,function(err,result){
             if (err){
                res.status(500).send(err.tostring());
            } 
